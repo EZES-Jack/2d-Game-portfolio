0227569d01f22158dee61324029d98f6fa8d01a3
EZES-Jack <jp.burns@outlook.com>
2025-01-31 17:42:48 +0000

    Update CameraFollowPlayer.cs

--- CameraFollowPlayer.cs
+++ CameraFollowPlayer.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class CameraFollowPlayer : MonoBehaviour
 {
-
+    // Transform component of the player to follow
     public Transform player;
 
     // Update is called once per frame
     void Update()
     {
+        // Set the camera position to follow the player's position
+        // The z-axis is set to -10 to keep the camera at a distance from the player
         transform.position = new Vector3(player.position.x, player.position.y, -10);
     }
 }
