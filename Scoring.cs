1948b6821284acec465a318f703d799c1fd9f453
EZES-Jack <jp.burns@outlook.com>
2025-01-31 17:52:52 +0000

    Update Scoring.cs

--- Scoring.cs
+++ Scoring.cs
@@ -6,40 +6,51 @@ using UnityEngine.UI;
 
 public class Scoring : MonoBehaviour
 {
+    // Static variable to keep track of the total score
     public static int totalScore;                         
+    // UI Text component to display the score
     public Text scoreText;                                   
 
     void Start()
     {
+        // Initialize the scoreText with the current total score
         scoreText.text = "Score: " + Scoring.totalScore;      
     }
 
-   
+    // Method called when the collider enters a trigger collider
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Check if the collided object has the tag "P_PotionBlue2"
         if (collision.tag == "P_PotionBlue2")                  
         {                                                      
+            // Increment the total score by 1
             Scoring.totalScore += 1;                          
+            // Update the scoreText with the new total score
             scoreText.text = "Score: " + Scoring.totalScore;   
         }
+        // Check if the collided object has the tag "level3"
         else if (collision.tag == "level3")                 
         {
+            // Increment the total score by 1
             Scoring.totalScore += 1;                          
+            // Update the scoreText with the new total score
             scoreText.text = "Score: " + Scoring.totalScore;   
-
         }
-
+        // Check if the collided object has the tag "end game +3"
         else if (collision.tag == "end game +3")             
         {
+            // Increment the total score by 3
             Scoring.totalScore += 3;                         
+            // Update the scoreText with the new total score
             scoreText.text = "Score: " + Scoring.totalScore;  
-
         }
+        // Check if the collided object has the tag "end game+10"
         else if (collision.tag == "end game+10")              
         {
+            // Increment the total score by 10
             Scoring.totalScore += 10;                         
+            // Update the scoreText with the new total score
             scoreText.text = "Score: " + Scoring.totalScore; 
-
         }
     }
 }
