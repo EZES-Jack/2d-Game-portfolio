398cd38f019b50d700f9511584d634c5285149f4
EZES-Jack <jp.burns@outlook.com>
2025-01-31 17:50:17 +0000

    Update Timer.cs

--- Timer.cs
+++ Timer.cs
@@ -5,18 +5,22 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
+    // Static variable to keep track of the elapsed time
     public static float timer;
+    // UI Text component to display the timer
     public Text timerText;
 
     void Start()
     {
+        // Initialize the timerText with the current timer value
         timerText.text = "time: " + Timer.timer;
     }
 
     void Update()
     {
+        // Update the timer with the time elapsed since the start of the game
         Timer.timer = Time.time;
+        // Update the timerText with the rounded timer value
         timerText.text = "Time: " + Mathf.RoundToInt(Timer.timer);
     }
-    
 }
